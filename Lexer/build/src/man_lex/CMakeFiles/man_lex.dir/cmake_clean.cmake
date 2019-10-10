8d1eda162ed6d9a0ab297bb4dee9610b9823543c
oliverdd <QAQddbest@outlook.com>
2019-10-10 13:35:34 +0800

    工程架构完成

---
+++ Lexer/build/src/man_lex/CMakeFiles/man_lex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../../bin/man_lex"
+  "../../../bin/man_lex.pdb"
+  "CMakeFiles/man_lex.dir/Main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/man_lex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
